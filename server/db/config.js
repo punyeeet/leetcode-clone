3689d0612aae6685ddd8eb176dc79007f1495fdf
punyeeet <puneetpradhan007@gmail.com>
2023-07-19 06:25:09 +0530

    databse url modified

--- server/db/config.js
+++ server/db/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const DATABASE = process.env.DATABASE;
 
 const connectDB = ()=>{
-    mongoose.connect(DATABASE).then(()=>console.log("connected to backend"));
+    mongoose.connect(`${DATABASE}`).then(()=>console.log("connected to backend"));
 }
   
 connectDB();
