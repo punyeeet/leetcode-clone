5ed7528ec5822f842092899e8761c8508032ab88
punyeeet <puneetpradhan007@gmail.com>
2023-07-19 12:07:11 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // import express from 'express'
-const cors = require('cors')
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
